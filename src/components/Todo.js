48b8e0d3bb681f084c16f9b7fd1cd5377b456af1
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-03-20 21:31:28 +0530

    Create Todo.js

---
+++ src/components/Todo.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import React from "react";
+
+export default function Todo() {
+  const [todo, setTodo] = useState([]);
+  const [temp, setTemp] = useState("");
+
+  function handleClick() {
+    // e.preventDefault();
+    setTodo([...todo, temp]);
+    setTemp("");
+  }
+
+  function handleDelete(s) {
+    setTodo((todos) => todos.filter((todo) => todo != s));
+  }
+
+  return (
+    <div>
+      <h2>To-Do List</h2>
+      <input
+        type="text"
+        value={temp}
+        onChange={(e) => setTemp(e.target.value)}
+      />
+      <button onClick={handleClick}>Add Todo</button>
+      <div>
+        {todo?.map((item, index) => (
+          <li>
+            {item}
+            <button onClick={(e) => handleDelete(item)}>Delete</button>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
+}
